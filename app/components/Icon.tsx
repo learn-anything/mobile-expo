f0c1de2ea47c35760cecfff77b1e13d4601deabf
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-10-22 14:11:16 +0200

    .

---
+++ app/components/Icon.tsx
@@ -0,0 +1,114 @@
+import * as React from "react"
+import { ComponentType } from "react"
+import {
+  Image,
+  ImageStyle,
+  StyleProp,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+  ViewProps,
+  ViewStyle,
+} from "react-native"
+
+export type IconTypes = keyof typeof iconRegistry
+
+interface IconProps extends TouchableOpacityProps {
+  /**
+   * The name of the icon
+   */
+  icon: IconTypes
+
+  /**
+   * An optional tint color for the icon
+   */
+  color?: string
+
+  /**
+   * An optional size for the icon. If not provided, the icon will be sized to the icon's resolution.
+   */
+  size?: number
+
+  /**
+   * Style overrides for the icon image
+   */
+  style?: StyleProp<ImageStyle>
+
+  /**
+   * Style overrides for the icon container
+   */
+  containerStyle?: StyleProp<ViewStyle>
+
+  /**
+   * An optional function to be called when the icon is pressed
+   */
+  onPress?: TouchableOpacityProps["onPress"]
+}
+
+/**
+ * A component to render a registered icon.
+ * It is wrapped in a <TouchableOpacity /> if `onPress` is provided, otherwise a <View />.
+ *
+ * - [Documentation and Examples](https://github.com/infinitered/ignite/blob/master/docs/Components-Icon.md)
+ */
+export function Icon(props: IconProps) {
+  const {
+    icon,
+    color,
+    size,
+    style: $imageStyleOverride,
+    containerStyle: $containerStyleOverride,
+    ...WrapperProps
+  } = props
+
+  const isPressable = !!WrapperProps.onPress
+  const Wrapper = (WrapperProps?.onPress ? TouchableOpacity : View) as ComponentType<
+    TouchableOpacityProps | ViewProps
+  >
+
+  const $imageStyle: StyleProp<ImageStyle> = [
+    $imageStyleBase,
+    color !== undefined && { tintColor: color },
+    size !== undefined && { width: size, height: size },
+    $imageStyleOverride,
+  ]
+
+  return (
+    <Wrapper
+      accessibilityRole={isPressable ? "imagebutton" : undefined}
+      {...WrapperProps}
+      style={$containerStyleOverride}
+    >
+      <Image style={$imageStyle} source={iconRegistry[icon]} />
+    </Wrapper>
+  )
+}
+
+export const iconRegistry = {
+  back: require("../../assets/icons/back.png"),
+  bell: require("../../assets/icons/bell.png"),
+  caretLeft: require("../../assets/icons/caretLeft.png"),
+  caretRight: require("../../assets/icons/caretRight.png"),
+  check: require("../../assets/icons/check.png"),
+  clap: require("../../assets/icons/demo/clap.png"), // @demo remove-current-line
+  community: require("../../assets/icons/demo/community.png"), // @demo remove-current-line
+  components: require("../../assets/icons/demo/components.png"), // @demo remove-current-line
+  debug: require("../../assets/icons/demo/debug.png"), // @demo remove-current-line
+  github: require("../../assets/icons/demo/github.png"), // @demo remove-current-line
+  heart: require("../../assets/icons/demo/heart.png"), // @demo remove-current-line
+  hidden: require("../../assets/icons/hidden.png"),
+  ladybug: require("../../assets/icons/ladybug.png"),
+  lock: require("../../assets/icons/lock.png"),
+  menu: require("../../assets/icons/menu.png"),
+  more: require("../../assets/icons/more.png"),
+  pin: require("../../assets/icons/demo/pin.png"), // @demo remove-current-line
+  podcast: require("../../assets/icons/demo/podcast.png"), // @demo remove-current-line
+  settings: require("../../assets/icons/settings.png"),
+  slack: require("../../assets/icons/demo/slack.png"), // @demo remove-current-line
+  view: require("../../assets/icons/view.png"),
+  x: require("../../assets/icons/x.png"),
+}
+
+const $imageStyleBase: ImageStyle = {
+  resizeMode: "contain",
+}
